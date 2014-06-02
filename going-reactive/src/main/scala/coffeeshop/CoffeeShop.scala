4678b53d34f29f2b766e91f81fcb48768fb42976
Kurt Rossgård <kurt.rossgard@gmail.com>
2014-06-02 12:41:10 +0200

    Removed solutions

--- going-reactive/src/main/scala/coffeeshop/CoffeeShop.scala
+++ going-reactive/src/main/scala/coffeeshop/CoffeeShop.scala
@@ -19,32 +19,7 @@ object Barista {
   object CloseShop
 }
 
-class Barista extends Actor with ActorLogging {
-  import akka.event.LoggingReceive
-  import coffeeshop.Barista._
-
-  var cappuccinoCount, espressoCount = 0
-
-  def serving: Receive = LoggingReceive {
-    case CappuccinoRequest => {
-      sender ! Bill(350)
-      cappuccinoCount += 1
-      log.info(s"Preparing cappuccino #${cappuccinoCount}")
-    }
-    case EspressoRequest => {
-      sender ! Bill(250)
-      espressoCount += 1
-      log.info(s"Preparing espresso #${espressoCount}")
-    }
-    case GetServedCoffeeRequests => sender ! ServedCoffeeRequests(espressoCount, cappuccinoCount)
-    case Lunch => context.become(lunchBreak)
-    case CloseShop => context.stop(self)
-  }
-
-  def lunchBreak: Receive = LoggingReceive {
-    case Work => context.become(serving)
-    case _ => sender ! LunchBreak
-  }
-
-  override def receive: Receive = serving
+class Barista extends Actor {
+    // TODO
+    override def receive: Receive = ???
 }
--- going-reactive/src/main/scala/supervision/SupervisionSample.scala
+++ going-reactive/src/main/scala/supervision/SupervisionSample.scala
@@ -9,13 +9,8 @@ object SupervisionSample {
     import akka.actor.SupervisorStrategy._
     import scala.concurrent.duration._
 
-    override val supervisorStrategy =
-      OneForOneStrategy(maxNrOfRetries = 10, withinTimeRange = 1 minute) {
-        case _: ArithmeticException      => Resume
-        case _: NullPointerException     => Restart
-        case _: IllegalArgumentException => Stop
-        case _: Exception                => Escalate
-      }
+    // TODO
+    override val supervisorStrategy = ???
 
     def receive = {
       case p: Props => sender ! context.actorOf(p)
@@ -27,33 +22,16 @@ object SupervisionSample {
     import akka.actor.SupervisorStrategy._
     import scala.concurrent.duration._
 
-    override val supervisorStrategy =
-      OneForOneStrategy(maxNrOfRetries = 10, withinTimeRange = 1 minute) {
-        case _: ArithmeticException      => Resume
-        case _: NullPointerException     => Restart
-        case _: IllegalArgumentException => Stop
-        case _: Exception                => Escalate
-      }
+    // TODO
+    override val supervisorStrategy = ???
 
     def receive = {
       case p: Props => sender() ! context.actorOf(p)
     }
-    // override default to kill all children during restart
-    override def preRestart(cause: Throwable, msg: Option[Any]) {}
-
-    override def preStart(): Unit = super.preStart()
-
-    override def postStop(): Unit = super.postStop()
-
-    override def postRestart(reason: Throwable): Unit = super.postRestart(reason)
   }
 
   class Child extends Actor {
-    var state = 0
-    def receive = {
-      case ex: Exception => throw ex
-      case x: Int        => state = x
-      case "get"         => sender ! state
-    }
+    // TODO
+    def receive = ???
   }
 }
